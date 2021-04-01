4ecaa7064d1943a0deb0e854313ee6471813a486
jorge.fuertes <jorge@jorgefuertes.com>
2021-04-01 05:40:57 +0200

    Config hotfix

--- internal/cfg/cfg.go
+++ internal/cfg/cfg.go
@@ -2,13 +2,12 @@ package cfg
 
 // MainConfig - main config type
 type MainConfig struct {
-	Version   bool   `flag optional name:"version" short:"v" help:"Display version and exit." type:"flag"`
-	Normalize bool   `flag optional name:"normalize" short:"n" help:"Normalization on/off (on). " type:"flag" default:"true"`
-	Verbose   bool   `flag optional name:"verbose" short:"b" help:"Verbosity (off)." type:"flag" default:"false"`
-	InFile    string `arg optional name:"in" help:"Input .wav file." type:"path"`
-	ReSample  uint32 `flag optional name:"resample" short:"r" help:"Down sample to 22050 or 11025 from greather multiple freq." default:0`
-	OutFile   string `arg optional name:"out" help:"Output .tzx file. Defaults to out.tzx." type:"path"`
-	Title     string `arg optional name:"title" help:"Archite title. Defaults to out filename."`
+	Version  bool   `flag optional name:"version" short:"v" help:"Display version and exit." type:"flag"`
+	Verbose  bool   `flag optional name:"verbose" short:"b" help:"Verbosity (off)." type:"flag" default:"false"`
+	InFile   string `arg optional name:"in" help:"Input .wav file." type:"path"`
+	ReSample uint32 `flag optional name:"resample" short:"r" help:"Down sample to 22050 or 11025 from greater multiple freq." default:0`
+	OutFile  string `arg optional name:"out" help:"Output .tzx file. Defaults to out.tzx." type:"path"`
+	Title    string `flag optional name:"title" short:"t" help:"Archite title. Defaults to out filename."`
 }
 
 // Main - Main configuration
