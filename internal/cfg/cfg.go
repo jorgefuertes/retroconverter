841cb542676f070456f06e923a58f47ebac0b494
jorge.fuertes <jorge@jorgefuertes.com>
2021-04-01 18:55:09 +0200

    Progress indicators

--- internal/cfg/cfg.go
+++ internal/cfg/cfg.go
@@ -4,7 +4,7 @@ package cfg
 type MainConfig struct {
 	Version  bool   `flag optional name:"version" short:"v" help:"Display version and exit." type:"flag"`
 	Verbose  bool   `flag optional name:"verbose" short:"b" help:"Verbosity (off)." type:"flag" default:"false"`
-	Progress bool   `flag optional name:"progress" short:"p" help:"Show progress (true)." type:"flag" default:"true"`
+	Progress bool   `flag optional name:"progress" short:"p" help:"Show progress (on)." type:"flag" default:"true"`
 	Title    string `flag optional name:"title" short:"t" help:"Archite title. Defaults to out filename."`
 	ReSample uint32 `flag optional name:"resample" short:"r" help:"Down sample to 22050 or 11025 from greater multiple freq." default:0`
 	InFile   string `arg optional name:"in" help:"Input .wav file." type:"path"`
